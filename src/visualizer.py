dc047848b55896aa717e8a7cad9b3ee7a11af2d9
mnsosa <mnsosa.ms@gmail.com>
2022-10-24 03:16:38 -0300

    pep8 fix

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,47 +1,50 @@
-# visualizer using streamlit
-from sz_utils import data_handler
-from config import config
-import streamlit as st
-import pandas as pd
+"""A visualizer using streamlit for easy implementation of the project
+
+This module is the main module of the visualizer. It uses the streamlit library
+to create a web app that can be used to visualize some data. You can select a
+patient and a edf file and then you can visualize the data in different ways.
+
+This module uses the data_handler module to get the data from the files.
+"""
+
 import mne
-import os
+import streamlit as st
+from sz_utils import data_handler
 
 
 def select_patient() -> str:
     """From the sidebar select a patient"""
-    option = st.sidebar.selectbox(
-        'Select a patient',
-        data_handler.get_patients())
+    option = st.sidebar.selectbox("Select a patient", data_handler.get_patients())
     return option
 
 
 def select_edf(patient) -> str:
     """From the sidebar select a edf file"""
     option = st.sidebar.selectbox(
-        'Select an edf file',
-        data_handler.get_patient_edf(patient))
+        "Select an edf file", data_handler.get_patient_edf(patient)
+    )
     return option
 
 
 def select_view() -> str:
     """From the sidebar select a view"""
     option: str = st.sidebar.selectbox(
-        'Select a view',
-        ['Table', 'Graph', 'Spectrogram'])
+        "Select a view", ["Table", "Graph", "Spectrogram"]
+    )
     return option
 
 
-# TODO
-def plot_spectrogram(edf: str) -> None:
-    pass
+def plot_spectrogram(edf: mne.io.edf.edf.RawEDF) -> None:
+    """Plot the spectrogram of the edf file in streamlit"""
+    raise NotImplementedError
 
 
 def main_view(edf_data: mne.io.edf.edf.RawEDF) -> None:
     """Main view of the app"""
     view: str = select_view()
-    if view == 'Graph':
+    if view == "Graph":
         pass
-    elif view == 'Spectrogram':
+    elif view == "Spectrogram":
         plot_spectrogram(edf_data)
     else:
         st.dataframe(edf_data.to_data_frame())
@@ -50,7 +53,7 @@ def main_view(edf_data: mne.io.edf.edf.RawEDF) -> None:
 
 def app() -> None:
     """Main function of the app"""
-    st.set_page_config(page_title='Seizure Prediction', page_icon=':brain:')
+    st.set_page_config(page_title="Seizure Prediction", page_icon=":brain:")
     st.title("EEG Data Visualizer")
 
     st.sidebar.title("Select your EEG data")
@@ -59,12 +62,10 @@ def app() -> None:
     edf: str = select_edf(patient)
     edf_data: mne.io.edf.edf.RawEDF = data_handler.get_edf_data(patient, edf)
 
-    main_view(edf_data) 
+    main_view(edf_data)
 
     st.write()
 
 
 if __name__ == "__main__":
     app()
-    
-    
