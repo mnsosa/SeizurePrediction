a031384f5efebd9aa6d2f51583d5198850484d05
MorillaGit <cristian_morilla@mi.unc.edu.ar>
2023-03-13 13:45:11 -0300

    MINUS fix

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
 config = {
-    "CHB_FOLDER_DIR": "/mnt/e/Documents/projects/seizure-prediction/data/chb-mit-scalp-eeg-database-1.0.0"
+    # "CHB_FOLDER_DIR": "/mnt/e/Documents/projects/seizure-prediction/data/chb-mit-scalp-eeg-database-1.0.0"
+    "CHB_FOLDER_DIR": "C:/Users/crist/Documents/tesis/data/chb-mit-scalp-eeg-database-1.0.0"
 }
